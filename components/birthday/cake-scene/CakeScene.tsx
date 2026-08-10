8269c27754267c0806ae6db93c264bd666ea4469
n7esports <n7esportspubgm@gmail.com>
2026-08-10 18:46:48 +0500

    Update CakeScene.tsx

--- components/birthday/cake-scene/CakeScene.tsx
+++ components/birthday/cake-scene/CakeScene.tsx
@@ -1,1 +1,25 @@
+"use client"
 
+import { useCandleState } from "./useCandleState"
+import Cake from "./Cake"
+import BlowButton from "./BlowButton"
+import BalloonSystem from "./BalloonSystem"
+import ConfettiSystem from "./ConfettiSystem"
+import BackgroundDecor from "./BackgroundDecor"
+
+export default function CakeScene() {
+  const { blown, blowCandles } = useCandleState()
+
+  return (
+    <div className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden text-white">
+      <BackgroundDecor />
+
+      <Cake blown={blown} />
+
+      <BlowButton onBlow={blowCandles} />
+
+      <BalloonSystem show={blown} />
+      <ConfettiSystem show={blown} />
+    </div>
+  )
+}
