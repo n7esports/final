e1e4deea4d80df13bdcb8082fb9e0a9f1b8716f7
n7esports <n7esportspubgm@gmail.com>
2026-08-10 18:44:51 +0500

    Update Candle.tsx

--- components/birthday/cake-scene/Candle.tsx
+++ components/birthday/cake-scene/Candle.tsx
@@ -1,1 +1,10 @@
+"use client"
+import Flame from "./Flame"
 
+export default function Candle({ isOff }: { isOff: boolean }) {
+  return (
+    <div className="relative w-2 h-10 bg-white rounded">
+      <Flame isOff={isOff} />
+    </div>
+  )
+}
